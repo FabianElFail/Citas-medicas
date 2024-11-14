036e4088159b52672b135c3b09d405df96d2cf73
FabianElFail <fabianecasasolivos@hotmail.com>
2024-11-14 08:07:08 -0500

    Primeras clases terminadas

--- Citas médicas/Cita.cs
+++ Citas médicas/Cita.cs
@@ -8,5 +8,25 @@ namespace Citas_médicas
 {
 	internal class Cita
 	{
-	}
+		public int Num { get; set; }
+		public Estudiante Est { get; set; }
+		public string Enfermedad { get; set; }
+		public double Precio { get; set; }
+
+		public Cita(int num, Estudiante est, string enfermedad, double precio)
+		{
+			Num = num;
+			Est = est;
+			Enfermedad = enfermedad;
+			Precio = precio;
+		}
+
+		public override string ToString()
+		{
+			return $"Número de Cita: {Num}, {Est}, Enfermedad: {Enfermedad}, Precio: {Precio:C}";
+		}
+	
+
+}
+
 }
--- Citas médicas/Estudiante.cs
+++ Citas médicas/Estudiante.cs
@@ -8,5 +8,23 @@ namespace Citas_médicas
 {
 	internal class Estudiante
 	{
+		public int Codigo { get; set; }
+		public string Nombre { get; set; }
+		public string Universidad { get; set; }
+
+		public Estudiante(int codigo, string nomb, string uni)
+		{
+			Codigo = codigo;
+			Nombre = nomb;
+			Universidad = uni;
+		}
+
+		public override string ToString()
+		{
+			return $"Código: {Codigo}, Nombre: {Nombre}, Universidad: {Universidad}";
+		}
 	}
+
+}
+
 }
